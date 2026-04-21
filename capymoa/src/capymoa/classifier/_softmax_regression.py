f96f23b3c8020339a85f27b5627dc2295991749f
chris-carstens <ccarstens256@gmail.com>
2026-04-22 00:34:20 +0200

    feat: add bias

--- capymoa/src/capymoa/classifier/_softmax_regression.py
+++ capymoa/src/capymoa/classifier/_softmax_regression.py
@@ -5,8 +5,7 @@ from capymoa.base import MOAClassifier
 from capymoa.stream import Schema
 from capymoa._utils import build_cli_str_from_mapping_and_locals
 
-# Import the Java MOA class via the JPype bridge
-import moa.classifiers.functions as moa_functions
+import moa.classifiers as moa_classifiers
 
 
 class SoftmaxRegression(MOAClassifier):
@@ -32,25 +31,31 @@ class SoftmaxRegression(MOAClassifier):
         schema: typing.Union[Schema, None] = None,
         random_seed: int = 0,
         learning_rate: float = 0.01,
+        bias_learning_rate: float = 0.01,
         l2_penalty: float = 0.0,
+        bias_init: float = 0.0,
     ):
         """Construct Softmax Regression.
 
         :param schema: Stream schema.
         :param random_seed: Seed for reproducibility.
         :param learning_rate: The learning rate for the SGD optimizer.
+        :param bias_learning_rate: The learning rate for the intercept (bias).
         :param l2_penalty: The L2 regularization weight.
+        :param bias_init: Initial value for the intercept (bias).
         """
         mapping = {
             "learning_rate": "-r",
-            "l2_penalty": "-l"
+            "bias_learning_rate": "-b",
+            "l2_penalty": "-l",
+            "bias_init": "-i",
         }
 
         config_str = build_cli_str_from_mapping_and_locals(mapping, locals())
 
         # Initialize the parent MOAClassifier
         super(SoftmaxRegression, self).__init__(
-            moa_learner=moa_functions.SoftmaxRegression,
+            moa_learner=moa_classifiers.SoftmaxRegression,
             schema=schema,
             CLI=config_str,
             random_seed=random_seed,
