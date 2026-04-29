7636e208fa1254cea5be4244b0981d6a9ea5c93f
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2026-04-29 17:18:01 +0200

    Revert "refactor: rename LogisticRegressionClassifier to LogisticRegression and add string representation"
    This reverts commit c9d4cd6fd65ee7ac33747d5ae6d17ec78e44623e.

--- capymoa/src/capymoa/classifier/_logistic_regression.py
+++ capymoa/src/capymoa/classifier/_logistic_regression.py
@@ -55,11 +55,8 @@ class LogisticRegression(MOAClassifier):
         config_str = build_cli_str_from_mapping_and_locals(mapping, locals())
 
         super(LogisticRegression, self).__init__(
-            moa_learner=moa_classifiers.LogisticRegression,
+            moa_learner=moa_classifiers.LogisticRegressionClassifier,
             schema=schema,
             CLI=config_str,
             random_seed=random_seed,
         )
-
-    def __str__(self):
-        return "LogisticRegression CapyMOA Classifier"
